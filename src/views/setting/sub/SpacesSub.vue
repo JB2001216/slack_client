37278f9f294aa7e04f3e51c180cd944374a1176d
JB2001216 <zhangxing2002611@gmail.com>
2019-09-24 19:34:25 -0400

    added space general view
    (cherry picked from commit 7d37c1dcbc01f84e1d92b05642e8dcc84bbd3075)

--- src/views/setting/sub/SpacesSub.vue
+++ src/views/setting/sub/SpacesSub.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="option_subColumn">
-    <p class="option_subColumn_title">{{$t('views.setting.sub.spaces.general')}}</p>
-    <button class="option_subColumn_button green active">{{$t('views.setting.sub.spaces.members')}}</button>
+    <template v-for="(item, $index) in menu">
+      <button
+        :key="`item-${$index}-b`"
+        :class="{active: item.routeName === currentRouterName}"
+        class="option_subColumn_button green"
+        @click="$store.mutations.settingRouter.to(item.routeName)">{{item.title()}}</button>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
+import { TranslateResult } from 'vue-i18n';
+import i18n from '@/i18n';
 
 @Component
 export default class SpacesSub extends Vue {
+
+  menu: {
+    title: () => TranslateResult;
+    routeName: string;
+  }[] = [
+    {
+      title: () => i18n.t('views.setting.sub.spaces.general'),
+      routeName: 'space-general',
+    },
+    {
+      title: () => i18n.t('views.setting.sub.spaces.members'),
+      routeName: 'space-members',
+    },
+  ];
+
+  get currentRouterName() {
+    return this.$store.state.settingRouter.name;
+  }
+
 }
 </script>
