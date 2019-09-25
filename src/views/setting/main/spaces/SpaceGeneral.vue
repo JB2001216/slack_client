93da3103e450360349d4b45c7911ad9fe10c5ec4
JB2001216 <zhangxing2002611@gmail.com>
2019-09-25 16:14:28 -0400

    updated upload structure

--- src/views/setting/main/spaces/SpaceGeneral.vue
+++ src/views/setting/main/spaces/SpaceGeneral.vue
@@ -8,11 +8,20 @@
 
       <div class="option_spaceGeneral_img">
         <div class="option_spaceGeneral_img_thumbnail">
-          <img src="~@/assets/images/parts/img_option_space_sample.jpg" alt="pic">
+          <input type="file" accept="image/png, .jpeg, .jpg"
+                 @change="inputFileChange">
+          <img v-if="url" :src="url" alt="pic">
+          <img v-if="!url" src="~@/assets/images/parts/img_option_space_sample.jpg" alt="pic">
         </div>
-        <button>
-          {{$t('views.setting.main.spaceGeneral.uploadBtn')}}
-        </button>
+        <div/><!-- separator -->
+        <div class="option_spaceGeneral_uploadButton">
+          <input type="file" accept="image/png, .jpeg, .jpg"
+                 @drop.prevent
+                 @change="inputFileChange">
+          <button>
+            {{$t('views.setting.main.spaceGeneral.uploadBtn')}}
+          </button>
+        </div>
         <p>{{$t('views.setting.main.spaceGeneral.uploadDescription')}}</p>
       </div>
 
@@ -38,5 +47,27 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class SpaceGeneral extends Vue {
+
+  url: string = '';
+  file: any;
+
+  async inputFileChange(event: any) {
+
+    const target = event.target;
+
+    if (target.files.length === 0) return;
+
+    this.file = target.files[0];
+
+    const myReader: FileReader = new FileReader();
+
+    myReader.onloadend = (event: any) => {
+      this.url = event.target.result;
+    };
+
+    myReader.readAsDataURL(this.file);
+
+  }
+
 }
 </script>
