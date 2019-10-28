616818f6aeaf41752e35e12fcba2faf8be9e8c09
JB2001216 <zhangxing2002611@gmail.com>
2019-10-28 04:52:10 +0200

    Add space notifications

--- src/events-subscription.ts
+++ src/events-subscription.ts
@@ -1,6 +1,7 @@
 import store from '@/store/index';
 import { appEventBus } from '@/plugins/app-event';
 import i18n from '@/i18n';
+import { apiRegistry, SpacesApi, SpaceUser } from '@/lib/api/';
 
 class EventsSubscription {
 
@@ -28,6 +29,185 @@ class EventsSubscription {
     };
 
     // message event
+    this.source.addEventListener('updateSpace', (e: any) => {
+      console.log('updateSpace');
+      const spacesApi = apiRegistry.load(SpacesApi, myUser.token);
+      spacesApi.spacesSpaceIdGet({
+        spaceId: myUser.space.id,
+      }).then((result) => {
+        console.log(result);
+        store.mutations.editSpace(result);
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+
+    this.source.addEventListener('deleteSpace', (e: any) => {
+      console.log('deleteSpace');
+      const spacesApi = apiRegistry.load(SpacesApi, myUser.token);
+      spacesApi.spacesSpaceIdDelete({
+        spaceId: myUser.space.id,
+      }).then((result) => {
+        console.log(result);
+        // DO UPDATE HERE
+
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+
+    this.source.addEventListener('updateMyUser', (e: any) => {
+      console.log('updateMyUser');
+      const data = JSON.parse(e.data);
+      const spacesApi = apiRegistry.load(SpacesApi, myUser.token);
+      // spacesApi.spacesSpaceIdUsersUserIdGet({
+      //   spaceId: myUser.space.id,
+      //   userId: data.params.userId,
+      // }).then((result) => {
+      //   console.log(result);
+      //   store.mutations.editMyUser({
+      //     id: result.id,
+      //     account: result.account,
+      //     displayName: result.displayName,
+      //     email: result.email,
+      //     spaceRoleId: result.spaceRoleId,
+      //     avatarUrl: result.avatarUrl,
+      //     avatarSmallUrl: result.avatarSmallUrl,
+      //     locale: 'en',
+      //     timezone: 'asdfasdfsafdsa',
+      //     space: {
+      //       id: result.spaceId,
+      //       account: result.account,
+      //       displayName: result.displayName,
+      //       avatarUrl: result.avatarUrl,
+      //       avatarSmallUrl: result.avatarSmallUrl,
+      //     },
+      //   });
+      //   // DO UPDATE HERE
+
+      // }).catch((error) => {
+      //   console.log(error);
+      // });
+    });
+
+    this.source.addEventListener('createSpaceUser', (e: any) => {
+      console.log('createSpaceUser');
+      const data = JSON.parse(e.data);
+      const spacesApi = apiRegistry.load(SpacesApi, myUser.token);
+      spacesApi.spacesSpaceIdUsersUserIdGet({
+        spaceId: myUser.space.id,
+        userId: data.params.userId,
+      }).then((result) => {
+        console.log(result);
+        store.mutations.activeUser.addSpaceUser(result);
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+
+    this.source.addEventListener('updateSpaceUser', (e: any) => {
+      console.log('updateSpaceUser');
+      const data = JSON.parse(e.data);
+      const spacesApi = apiRegistry.load(SpacesApi, myUser.token);
+      spacesApi.spacesSpaceIdUsersUserIdGet({
+        spaceId: myUser.space.id,
+        userId: data.params.userId,
+      }).then((result) => {
+        console.log(result);
+        store.mutations.activeUser.addSpaceUser(result);
+        // DO UPDATE HERE
+
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+
+    this.source.addEventListener('deleteSpaceUser', (e: any) => {
+      console.log('deleteSpaceUser');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('createProject', (e: any) => {
+      console.log('createProject');
+      const data = JSON.parse(e.data);
+      // store.mutations.activeUser.addProject(e);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('updateProject', (e: any) => {
+      console.log('updateProject');
+      const data = JSON.parse(e.data);
+      // store.mutations.activeUser.editProject(resObj.data.params);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('deleteProject', (e: any) => {
+      console.log('deleteProject');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('createProjectUser', (e: any) => {
+      console.log('createProjectUser');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('updateProjectUser', (e: any) => {
+      console.log('updateProjectUser');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('createTask', (e: any) => {
+      console.log('createTask');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('updateTask', (e: any) => {
+      console.log('updateTask');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('createTaskComment', (e: any) => {
+      console.log('createTaskComment');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('updateTaskComment', (e: any) => {
+      console.log('updateTaskComment');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('deleteTaskComment', (e: any) => {
+      console.log('deleteTaskComment');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('createNote', (e: any) => {
+      console.log('createNote');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('updateNote', (e: any) => {
+      console.log('updateNote');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
+    this.source.addEventListener('deleteNote', (e: any) => {
+      console.log('deleteNote');
+      const data = JSON.parse(e.data);
+      // DO UPDATE HERE
+    });
+
     this.source.onmessage = (res: any) => {
 
       const resObj: any = JSON.parse(res);
@@ -35,106 +215,16 @@ class EventsSubscription {
       // check currect user
       this.isFireUser = resObj.data.clientId === myUser.id;
 
+      console.log(resObj);
+
       switch (resObj.event) {
 
-        case 'updateSpace':
-          store.mutations.editSpace(resObj.data.params);
-          if (this.isFireUser) {
-            appEventBus.emit('flash', { 'message': i18n.t('views.setting.main.statusFlow.updatedMessage').toString(), 'name': 'success' });
-          }
-          break;
-
-        case 'deleteSpace':
-          // DO UPDATE HERE
-          break;
-
-        case 'updateMyUser':
-          store.mutations.editMyUser(resObj.data.params);
-          if (this.isFireUser) {
-            appEventBus.emit('flash', { 'message': i18n.t('views.setting.main.statusFlow.updatedMessage').toString(), 'name': 'success' });
-          }
-          break;
-
-        case 'createSpaceUser':
-          store.mutations.activeUser.addSpaceUser(resObj.data.params);
-          break;
-
-        case 'updateSpaceUser':
-          store.mutations.activeUser.editSpaceUser(resObj.data.params);
-          if (this.isFireUser) {
-            appEventBus.emit('flash', { 'message': i18n.t('views.setting.main.statusFlow.updatedMessage').toString(), 'name': 'success' });
-          }
-          break;
-
-        case 'deleteSpaceUser':
-          // DO UPDATE HERE
-          break;
-
-        case 'createProject':
-          store.mutations.activeUser.addProject(resObj.data.params);
-          break;
-
-        case 'updateProject':
-          store.mutations.activeUser.editProject(resObj.data.params);
-          if (this.isFireUser) {
-            appEventBus.emit('flash', { 'message': i18n.t('views.setting.main.statusFlow.updatedMessage').toString(), 'name': 'success' });
-          }
-          break;
-
-        case 'deleteProject':
-          // DO UPDATE HERE
-          break;
-
-        case 'createProjectUser':
-          // DO UPDATE HERE
-          break;
-
-        case 'updateProjectUser':
-          // DO UPDATE HERE
-          break;
-
-        case 'deleteProjectUser':
-          // DO UPDATE HERE
-          break;
-
-        case 'createTask':
-          // DO UPDATE HERE
-          break;
-
-        case 'updateTask':
-          // DO UPDATE HERE
-          break;
-
-        case 'deleteTask':
-          // DO UPDATE HERE
-          break;
-
-        case 'createTaskComment':
-          // DO UPDATE HERE
-          break;
-
-        case 'updateTaskComment':
-          // DO UPDATE HERE
-          break;
-
-        case 'deleteTaskComment':
-          // DO UPDATE HERE
-          break;
-
-        case 'createNote':
-          // DO UPDATE HERE
-          break;
-
-        case 'updateNote':
-          // DO UPDATE HERE
-          break;
-
-        case 'deleteNote':
-          // DO UPDATE HERE
-          break;
-
-        default:
-          break;
+        // case 'updateSpace':
+        //   store.mutations.editSpace(resObj.data.params);
+        //   if (this.isFireUser) {
+        //     appEventBus.emit('flash', { 'message': i18n.t('views.setting.main.statusFlow.updatedMessage').toString(), 'name': 'success' });
+        //   }
+        //   break;
 
       }
 
